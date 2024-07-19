edd177f8d727ee466e911475f33a8bcdca6376e7
Elijah Mendez <elijahmendez2022@gmail.com>
2024-07-19 18:07:01 -0400

    Small Changes --> 1:49:39

--- components/forms/RegisterForm.tsx
+++ components/forms/RegisterForm.tsx
@@ -133,12 +133,6 @@ const RegisterForm = ({ user }: { user: User }) => {
           />
         </div>
 
-        {/* <section className="space-y-6">
-            <div className="mb-9 space-y-1">
-              <h2 className="sub-header">Medical Information</h2>
-            </div>
-          </section> */}
-
         <div className="flex flex-col gap-6 xl:flex-row">
           <CustomFormField
             fieldType={FormFieldType.INPUT}
@@ -157,14 +151,12 @@ const RegisterForm = ({ user }: { user: User }) => {
         </div>
 
         <div className="flex flex-col gap-6 xl:flex-row">
-
-        <CustomFormField
+          <CustomFormField
             fieldType={FormFieldType.INPUT}
             control={form.control}
             name="emergencyContactName"
             label="Emergency Contact Name"
             placeholder="Guardian's name"
-            
           />
           <CustomFormField
             fieldType={FormFieldType.PHONE_INPUT}
@@ -174,6 +166,24 @@ const RegisterForm = ({ user }: { user: User }) => {
             placeholder=""
           />
         </div>
+
+          <section className="space-y-6">
+            <div className="mb-9 space-y-1">
+              <h2 className="sub-header">Medical Information</h2>
+            </div>
+          </section>
+
+          <CustomFormField
+            fieldType={FormFieldType.SELECT}
+            control={form.control}
+            name="primaryPhysician"
+            label="Primary Physician"
+            placeholder="Select a physician"
+            >
+              {['Dr. John Doe', 'Dr. Jane Doe', 'Dr. Alex Doe']}
+            </CustomFormField>
+          
+        
 
         <SubmitButton isLoading={isLoading}>Get Started</SubmitButton>
       </form>
