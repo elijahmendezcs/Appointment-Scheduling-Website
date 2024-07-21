07e731dce38b9e5368d40fafb6d86823051bf859
Elijah Mendez <elijahmendez2022@gmail.com>
2024-07-21 17:28:24 -0400

    Commit before I start for the day. 2:42:16

--- app/patients/[userId]/new-appointment/page.tsx
+++ app/patients/[userId]/new-appointment/page.tsx
@@ -1,7 +1,37 @@
-import React from "react";
+import AppointmentForm from "@/components/forms/AppointmentForm";
+import PatientForm from "@/components/forms/PatientForm";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
 
-const NewAppointment = () => {
-  return <div> New Appointment</div>;
-};
+export default function NewAppointment() {
+  return (
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[860px] flex-1 justify-between">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            height={1000}
+            width={1000}
+            alt="patient"
+            className="mb-12 h-10 w-fit"
+          />
 
-export default NewAppointment;
+          <AppointmentForm />
+
+          <p className="justify-items-end text-dark-600 xl:text-left">
+            © 2024 CarePulse
+          </p>
+        </div>
+      </section>
+
+      <Image
+        src="/assets/images/appointment-img.png"
+        height={1000}
+        width={1000}
+        alt="appointment"
+        className="side-img max-w-[390px] bg-bottom"
+      />
+    </div>
+  );
+}
