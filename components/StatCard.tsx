8b04a9c82a6ad4ac573d40331000a8c7a38d1779
Elijah Mendez <elijahmendez2022@gmail.com>
2024-07-24 16:53:30 -0400

    Small Change before going into backend 3:46:15

--- components/StatCard.tsx
+++ components/StatCard.tsx
@@ -26,7 +26,10 @@ const StatCard = ({ count = 0, label, icon, type }: StatCardProps) => {
           alt={label}
           className="size-8 w-fit"
         />
+
+        <h2 className="text-32-bold text-white">{count}</h2>
       </div>
+      <p className="text-14-regular">{label}</p>
     </div>
   );
 };
