e816ad612416700796df4f6906c0b69b9af773f8
Elijah Mendez <elijahmendez2022@gmail.com>
2024-07-24 15:28:53 -0400

    Small tweaks Starting admin page.

--- components/PasskeyModal.tsx
+++ components/PasskeyModal.tsx
@@ -17,7 +17,7 @@ import {
   InputOTPSeparator,
   InputOTPSlot,
 } from "@/components/ui/input-otp";
-import { encryptKey } from "@/lib/utils";
+import { decryptKey, encryptKey } from "@/lib/utils";
 import Image from "next/image";
 import { usePathname, useRouter } from "next/navigation";
 
@@ -36,8 +36,10 @@ const PasskeyModal = () => {
       : null;
 
   useEffect(() => {
+    const accessKey = encryptedKey && decryptKey(encryptedKey);
+
     if (path)
-      if (passkey === process.env.NEXT_PUBLIC_ADMIN_PASSKEY) {
+      if (accessKey === process.env.NEXT_PUBLIC_ADMIN_PASSKEY) {
         const encryptedKey = encryptKey(passkey);
 
         setOpen(false);
