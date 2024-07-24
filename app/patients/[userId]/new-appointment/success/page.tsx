82d444e3a440c74b943ff61c259f523201efffd0
Elijah Mendez <elijahmendez2022@gmail.com>
2024-07-24 14:11:08 -0400

    Commit before I start for the day. 3:15:48

--- app/patients/[userId]/new-appointment/success/page.tsx
+++ app/patients/[userId]/new-appointment/success/page.tsx
@@ -1,11 +1,24 @@
+import { Doctors } from "@/constants";
+import { getAppointment } from "@/lib/actions/appointment.actions";
+import { formatDateTime } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { formatDate } from "react-datepicker/dist/date_utils";
 
-const Success = () => {
+const Success = async ({
+  params: { userId },
+  searchParams,
+}: SearchParamProps) => {
+  const appointmentId = (searchParams?.appointmentId as string) || "";
+  const appointment = await getAppointment(appointmentId);
+
+  const doctor = Doctors.find(
+    (doc) => doc.name === appointment.primaryPhysician
+  );
+
   return (
     <div className="flex h-screen max-h-screen px-[5%]">
-      Success
       <div className="success-img">
         <Link href="/">
           <Image
@@ -34,11 +47,25 @@ const Success = () => {
         <section className="request-details">
           <p>Requested appointment details:</p>
           <div className="flex items-center gap-3">
-            {/* <Image 
+            <Image
+              src={doctor?.image!}
+              alt="doctor"
+              width={100}
+              height={100}
+              className="size-6"
+            />
 
-            /> */}
+            <p className="whitespace-nowrap">Dr. {doctor?.name}</p>
           </div>
-
+          <div className="flex gap-2">
+            <Image
+              src="/assets/icons/calendar.svg"
+              alt="calendar"
+              width={24}
+              height={24}
+            />
+            <p>{formatDateTime(appointment.schedule).dateTime}</p>
+          </div>
         </section>
       </div>
     </div>
