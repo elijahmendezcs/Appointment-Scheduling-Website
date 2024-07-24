4f544991153b8c4b21e6214aac5d153eac11a363
Elijah Mendez <elijahmendez2022@gmail.com>
2024-07-24 14:50:24 -0400

    OTP verification update. 3:28:05

--- components/PasskeyModal.tsx
+++ components/PasskeyModal.tsx
@@ -1,9 +1,81 @@
-import React from 'react'
+"use client";
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
+
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSeparator,
+  InputOTPSlot,
+} from "@/components/ui/input-otp";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+import React, { useState } from "react";
 
 const PasskeyModal = () => {
-  return (
-    <div>PasskeyModal</div>
-  )
-}
+  const router = useRouter();
+  const [open, setOpen] = useState(true);
+  const [passkey, setPasskey] = useState("");
 
-export default PasskeyModal
+  const closeModal = () => {
+    setOpen(false);
+    router.push("/");
+  };
+
+  return (
+    <AlertDialog open={open} onOpenChange={setOpen}>
+      <AlertDialogContent className="shad-alert-dialog">
+        <AlertDialogHeader>
+          <AlertDialogTitle className="flex items-start justify-between">
+            Admin Access Verification
+            <Image
+              src="/assets/icons/close.svg"
+              alt="close"
+              width={20}
+              height={20}
+              onClick={() => closeModal()}
+              className="cursor-pointer"
+            />
+          </AlertDialogTitle>
+          <AlertDialogDescription>
+            To access the admin page, please enter the passkey.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+
+        <div>
+          <InputOTP
+            maxLength={6}
+            value={passkey}
+            onChange={(value) => setPasskey(value)}
+          >
+            <InputOTPGroup className="shad-otp">
+              <InputOTPSlot className="shad-otp-slot" index={0} />
+              <InputOTPSlot className="shad-otp-slot" index={1} />
+              <InputOTPSlot className="shad-otp-slot" index={2} />
+              <InputOTPSlot className="shad-otp-slot" index={3} />
+              <InputOTPSlot className="shad-otp-slot" index={4} />
+              <InputOTPSlot className="shad-otp-slot" index={5} />
+            </InputOTPGroup>
+          </InputOTP>
+        </div>
+
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction>Continue</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
+
+export default PasskeyModal;
